95f4b0a9fe8178e280105f76db0a1fd5dd87070b
Matthias Perktold <tias251@gmail.com>
2018-06-17 13:46:46 +0200

    Fix stack overflow for pullable sources of many items

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const pump = inputSource => {
   return outputSource = ( start, outputSink ) => {
     if ( start === 0 ) {
       let talkback;
+      let pulling;
+      let pushReceived;
+
+      const pullWhilePushReceived = () => {
+        pulling = true;
+        do {
+          pushReceived = false;
+          talkback( 1 );
+        } while (pulling && pushReceived);
+        pulling = false;
+      }
 
       inputSource( 0, ( type, data ) => {
         if ( type === 0 ) {
@@ -9,17 +20,23 @@ const pump = inputSource => {
 
           outputSink( 0, ( t, d ) => {
             if ( t === 2 ) {
+              pulling = false;          // exit pull loop
               talkback( 2 );
             }
           });
 
-          talkback( 1 );
+          pullWhilePushReceived();
         }
         else {
           outputSink( type, data );
 
           if ( type === 1 ) {
-            talkback( 1 );
+            if ( pulling ) {
+              pushReceived = true;      // continue pull loop
+            }
+            else {
+              pullWhilePushReceived();  // start pull loop
+            }
           }
         }
       })
